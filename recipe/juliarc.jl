f00d2dbac95f65cff95aa3bf3f11dbcd2d673b81
Dan Fornika <dfornika@gmail.com>
2017-09-12 15:29:28 -0700

    Correct setting Base.LOAD_CACHE_PATH

--- recipe/juliarc.jl
+++ recipe/juliarc.jl
@@ -2,5 +2,5 @@ JULIA_PREFIX = abspath(joinpath(Base.source_path(), "..", "..", ".."))
 
 if !("JULIA_PKGDIR" in keys(ENV))
     ENV["JULIA_PKGDIR"] = joinpath(JULIA_PREFIX, "share", "julia", "site")
-    pop!(Base.LOAD_CACHE_PATH)
+    Base.LOAD_CACHE_PATH[1] = ENV["JULIA_PKGDIR"]
 end
