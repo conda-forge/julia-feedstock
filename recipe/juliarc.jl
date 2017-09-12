ceac1e1a98cf305bf024d8813f93ef4ecb9f5bec
Dan Fornika <dfornika@gmail.com>
2017-09-12 15:00:52 -0700

    Removed Pkg.init() from juliarc.jl

--- recipe/juliarc.jl
+++ recipe/juliarc.jl
@@ -2,7 +2,5 @@ JULIA_PREFIX = abspath(joinpath(Base.source_path(), "..", "..", ".."))
 
 if !("JULIA_PKGDIR" in keys(ENV))
     ENV["JULIA_PKGDIR"] = joinpath(JULIA_PREFIX, "share", "julia", "site")
-    Pkg.init()
-    Pkg.__init__()
     pop!(Base.LOAD_CACHE_PATH)
 end
