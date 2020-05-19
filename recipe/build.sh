245dd3cc2eff83fc17b8fc60dbdfa8d1bef25700
Matt Johnson <mjohnson541@gmail.com>
2020-05-19 12:37:28 -0400

    set JULIA_DEPOT_PATH when conda env activated

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,27 @@ export LIBRARY_PATH=${PREFIX}/lib
 export CMAKE_PREFIX_PATH=${PREFIX}
 export PATH="${PREFIX}/bin:${PATH}"
 
+#set JULIA_DEPOT_PATH in conda env
+export JULIA_DEPOT_PATH="${PREFIX}/share/julia/site:$JULIA_DEPOT_PATH" 
+ACTIVATE_ENV="${PREFIX}/etc/conda/activate.d/env_vars.sh"
+DEACTIVATE_ENV="${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+if [ -f "$ACTIVATE_ENV" ]; then
+	echo "export JULIA_DEPOT_PATH=\"${PREFIX}/share/julia/site:\$JULIA_DEPOT_PATH\"" >> "$ACTIVATE_ENV"
+else
+	mkdir -p ${PREFIX}/etc/conda/activate.d
+	touch ${PREFIX}/etc/conda/activate.d/env_vars.sh
+	echo '#!/bin/sh' >> $ACTIVATE_ENV
+	echo "export JULIA_DEPOT_PATH=\"${PREFIX}/share/julia/site:\$JULIA_DEPOT_PATH\"" >> "$ACTIVATE_ENV"
+fi
+if [ -f "$DEACTIVATE_ENV" ]; then
+	echo "unset JULIA_DEPOT_PATH" >> "$DEACTIVATE_ENV"
+else
+	mkdir -p ${PREFIX}/etc/conda/deactivate.d
+	touch ${PREFIX}/etc/conda/deactivate.d/env_vars.sh
+	echo '#!/bin/sh' >> "$DEACTIVATE_ENV"
+	echo "unset JULIA_DEPOT_PATH" >> "$DEACTIVATE_ENV"
+fi
+
 # Hack to suppress building docs
 cat > doc/Makefile << EOF
 html :	
