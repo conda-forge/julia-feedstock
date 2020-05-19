eb79265ab4e72a916d5ad25d35cb83989fd85585
Matt Johnson <mjohnson541@gmail.com>
2020-05-19 14:40:39 -0400

    simplify recipe and add back up variables

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,24 +8,12 @@ export PATH="${PREFIX}/bin:${PATH}"
 
 #set JULIA_DEPOT_PATH in conda env
 export JULIA_DEPOT_PATH="${PREFIX}/share/julia/site:$JULIA_DEPOT_PATH" 
-ACTIVATE_ENV="${PREFIX}/etc/conda/activate.d/env_vars.sh"
-DEACTIVATE_ENV="${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
-if [ -f "$ACTIVATE_ENV" ]; then
-	echo "export JULIA_DEPOT_PATH=\"${PREFIX}/share/julia/site:\$JULIA_DEPOT_PATH\"" >> "$ACTIVATE_ENV"
-else
-	mkdir -p ${PREFIX}/etc/conda/activate.d
-	touch ${PREFIX}/etc/conda/activate.d/env_vars.sh
-	echo '#!/bin/sh' >> $ACTIVATE_ENV
-	echo "export JULIA_DEPOT_PATH=\"${PREFIX}/share/julia/site:\$JULIA_DEPOT_PATH\"" >> "$ACTIVATE_ENV"
-fi
-if [ -f "$DEACTIVATE_ENV" ]; then
-	echo "unset JULIA_DEPOT_PATH" >> "$DEACTIVATE_ENV"
-else
-	mkdir -p ${PREFIX}/etc/conda/deactivate.d
-	touch ${PREFIX}/etc/conda/deactivate.d/env_vars.sh
-	echo '#!/bin/sh' >> "$DEACTIVATE_ENV"
-	echo "unset JULIA_DEPOT_PATH" >> "$DEACTIVATE_ENV"
-fi
+
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
 
 # Hack to suppress building docs
 cat > doc/Makefile << EOF
