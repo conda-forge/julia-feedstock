30a3e2fddbe45f80db022c1f13bfcfa8d98232e9
Isuru Fernando <isuruf@gmail.com>
2020-07-03 12:45:35 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,6 +23,8 @@ then
     export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBUNWIND=1"
     # Not sure why llvmdev is getting installed. Probably a conda solver bug
     rm -rf $BUILD_PREFIX/lib/libLLVM*.a
+    rm -rf $BUILD_PREFIX/include/llvm/*
+    rm -rf $BUILD_PREFIX/include/llvm-c/*
 elif [ "$(uname)" == "Linux" ]
 then
 	# On linux the released version of libunwind has issues building julia
