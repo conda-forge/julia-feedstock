cc03c81e7e1fed1e5c9e06f62d203f637a44a349
Isuru Fernando <isuruf@gmail.com>
2020-10-18 01:51:58 -0500

    Use libunwind from julia

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,18 +15,7 @@ EOF
 # Julia sets this to unix makefiles later on in its build process
 export CMAKE_GENERATOR="make"
 
-NO_GIT=1 make -C base version_git.jl.phony CC=$CC CXX=$CXX FC=$FC
-
-export EXTRA_MAKEFLAGS="" 
-if [ "$(uname)" == "Darwin" ]
-then
-    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBUNWIND=1"
-elif [ "$(uname)" == "Linux" ]
-then
-	# On linux the released version of libunwind has issues building julia
-	# See: https://github.com/JuliaLang/julia/issues/23615
-    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBUNWIND=0"
-fi
+NO_GIT=1 make -C base version_git.jl.phony CC=$CC CXX=$CXX FC=$FC V=1
 
 make -j 4 prefix=${PREFIX} MARCH=core2 sysconfigdir=${PREFIX}/etc NO_GIT=1 \
  LIBBLAS=-lblas LIBBLASNAME=libblas LIBLAPACK=-llapack LIBLAPACKNAME=liblapack \
@@ -45,7 +34,7 @@ make -j 4 prefix=${PREFIX} MARCH=core2 sysconfigdir=${PREFIX}/etc NO_GIT=1 \
  USE_SYSTEM_PATCHELF=1 \
  USE_SYSTEM_PCRE=1 \
  USE_SYSTEM_SUITESPARSE=1 \
- ${EXTRA_MAKEFLAGS}	\
+ USE_SYSTEM_LIBUNWIND=0	\
  TAGGED_RELEASE_BANNER="conda-forge-julia release" \
  CC=$CC CXX=$CXX FC=$FC \
  install
