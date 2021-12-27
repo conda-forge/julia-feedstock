0682a5fdac791eb2286f932b11fbfbe1477d87c8
ngam <67342040+ngam@users.noreply.github.com>
2021-12-27 08:49:16 -0500

    Revert "Re-add JULIA_PROJECT to be "@$CONDA_DEFAULT_ENV""
    This reverts commit 1e312a12bc4722885fcd896488bca53b2cedc838.

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,6 +1,3 @@
 export JULIA_DEPOT_PATH_BACKUP=${JULIA_DEPOT_PATH:-}
 export JULIA_PROJECT_BACKUP=${JULIA_PROJECT:-}
 export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
-# Use of @ to specify a shared named environment is new as of Julia 1.7
-# https://github.com/JuliaLang/julia/pull/40025
-export JULIA_PROJECT="@$CONDA_DEFAULT_ENV"
