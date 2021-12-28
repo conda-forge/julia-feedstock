3bff446bb37d832913cbdc90126d4f7c22956164
Mark Kittisopikul <markkitt@gmail.com>
2021-12-27 20:45:21 -0500

    Set JULIA_PROJECT and JULIA_LOAD_PATH according to conda environment

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,3 +1,13 @@
 export JULIA_DEPOT_PATH_BACKUP=${JULIA_DEPOT_PATH:-}
 export JULIA_PROJECT_BACKUP=${JULIA_PROJECT:-}
+export JULIA_LOAD_PATH_BACKUP=${JULIA_LOAD_PATH:-}
+
+# Move ~/.julia to a conda environment specific location
 export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
+
+# Create a unique shared named environment for each conda environment
+# Use of @ to specify a shared named environment is new as of Julia 1.7
+# https://github.com/JuliaLang/julia/pull/40025
+export JULIA_PROJECT="@$CONDA_DEFAULT_ENV"
+# Modify load path so that projects stack on the unique shared named environment
+export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
