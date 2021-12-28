03f4681eca04f17863b442d3e99526f1eb41e758
Mark Kittisopikul <markkitt@gmail.com>
2021-12-27 23:51:53 -0500

    Extract env name as last directory of CONDA_PREFIX

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -8,6 +8,7 @@ export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
 # Create a unique shared named environment for each conda environment
 # Use of @ to specify a shared named environment is new as of Julia 1.7
 # https://github.com/JuliaLang/julia/pull/40025
-export JULIA_PROJECT="@$CONDA_DEFAULT_ENV"
+# The name of the environment is the last directory of the CONDA_PREFIX
+export JULIA_PROJECT="@${CONDA_PREFIX##*/}"
 # Modify load path so that projects stack on the unique shared named environment
 export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
