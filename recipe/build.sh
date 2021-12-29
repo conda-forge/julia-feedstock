bd5133e708542ffdfb3ae5131636d3db3c58f4fa
ngam <67342040+ngam@users.noreply.github.com>
2021-12-29 17:41:23 -0500

    reorganize & cleanup (#157)
    * set JULIA_DEPOT_PATH when conda env activated
    
    * simplify recipe and add back up variables
    
    * don't run juliarc.jl
    
    * improve activate.d/deactivate.d file editing
    
    * Apply suggestions from code review by @mkitti
    
    * This uses the activate.sh and deactivate.sh scripts that backup the environment variable.
    * Create a new shared environment located in the site depot for the environment rather than continuing to use the default one in ~/.julia/environments/v#.#.
    
    * reorganize
    
    * increase bld no
    
    * try building osx with system mbedtls
    
    * only apply three new patches to linux for now
    
    * reorganize order of patches
    
    * Revert "try building osx with system mbedtls"
    
    This reverts commit 1b1d1708c486cdb9d92f9ea2199c3621e5f4963f.
    
    * comment out juliarc.jl
    
    * Update build.sh
    
    * try runtests math for osx
    
    * test "opaque_closure", "filesystem"
    
    * go all in on testing
    
    * revert previous; parallelize + add "math" to list
    
    - use all processors: ncores=ceil(Int, Sys.CPU_THREADS
    - move math up to regular tests (no longer necessary to stop it from running on osx)
    
    * Update build.sh
    
    * Update activate.sh
    
    * Revert "Update activate.sh"
    
    This reverts commit fe2b9c9e7c6ab9836fc34ccaadd6c97d13558fa6.
    
    * new patches
    
    * correct patches in meta
    
    * change of patches
    
    * Update meta.yaml
    
    * Modify patch to match upstream JuliaLang/julia#42358. Use addenv.
    
    * Re-add JULIA_PROJECT to be "@$CONDA_DEFAULT_ENV"
    
    * Revert "Re-add JULIA_PROJECT to be "@$CONDA_DEFAULT_ENV""
    
    This reverts commit 1e312a12bc4722885fcd896488bca53b2cedc838.
    
    * Set JULIA_PROJECT and JULIA_LOAD_PATH according to conda environment
    
    * Test loading and cmdlineargs with default JULIA_PROJECT
    
    * Extract env name as last directory of CONDA_PREFIX
    
    * Remove JULIA_LOAD_PATH while testing loading
    
    * Update build.sh
    
    * Delete recipe/old_unused directory
    
    * reorganize patches and delete unnecessary files
    
    * remove DS_STORE
    
    * oops place back patches
    
    * Apply suggestions from code review, remove "unique"
    
    Co-authored-by: ngam <67342040+ngam@users.noreply.github.com>
    
    Co-authored-by: Matt Johnson <mjohnson541@gmail.com>
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>
    Co-authored-by: Mark Kittisopikul <markkitt@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -62,9 +62,14 @@ make -j${CPU_COUNT} prefix=${PREFIX} sysconfigdir=${PREFIX}/etc \
  USE_SYSTEM_ZLIB=1 \
  USE_SYSTEM_P7ZIP=1 \
  ${EXTRA_MAKEFLAGS} \
- TAGGED_RELEASE_BANNER="conda-forge-julia release" \
+ TAGGED_RELEASE_BANNER="A conda-forge release: https://github.com/conda-forge/julia-feedstock" \
  CC=$CC CXX=$CXX FC=$FC \
  install
 
-# Configure juliarc to use conda environment
-cat "${RECIPE_DIR}/juliarc.jl" >> "${PREFIX}/etc/julia/juliarc.jl"
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
---
+++ recipe/scripts/activate.sh
@@ -0,0 +1,14 @@
+export JULIA_DEPOT_PATH_BACKUP=${JULIA_DEPOT_PATH:-}
+export JULIA_PROJECT_BACKUP=${JULIA_PROJECT:-}
+export JULIA_LOAD_PATH_BACKUP=${JULIA_LOAD_PATH:-}
+
+# Move ~/.julia to a conda environment specific location
+export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
+
+# Create a named environment for each conda environment
+# Use of @ to specify a shared named environment is new as of Julia 1.7
+# https://github.com/JuliaLang/julia/pull/40025
+# The name of the environment is the last directory of the CONDA_PREFIX
+export JULIA_PROJECT="@${CONDA_PREFIX##*/}"
+# Modify load path so that projects stack on the conda-named environment
+export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
