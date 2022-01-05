bc04a93506198f6f4461b95230d081b4b4dad8ab
ngam <67342040+ngam@users.noreply.github.com>
2022-01-05 14:28:24 -0500

    sync with 1.7.1

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -2,16 +2,13 @@ export JULIA_DEPOT_PATH_BACKUP=${JULIA_DEPOT_PATH:-}
 export JULIA_PROJECT_BACKUP=${JULIA_PROJECT:-}
 export JULIA_LOAD_PATH_BACKUP=${JULIA_LOAD_PATH:-}
 
-# Uncomment the export lines below for similar Julia depot behavior
-# as on the 1.7 branch of the conda-forge release
-
 # Move ~/.julia to a conda environment specific location
-#export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
+export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
 
 # Create a named environment for each conda environment
 # Use of @ to specify a shared named environment is new as of Julia 1.7
 # https://github.com/JuliaLang/julia/pull/40025
 # The name of the environment is the last directory of the CONDA_PREFIX
-#export JULIA_PROJECT="$CONDA_PREFIX/share/julia/environments/${CONDA_PREFIX##*/}"
+export JULIA_PROJECT="@${CONDA_PREFIX##*/}"
 # Modify load path so that projects stack on the conda-named environment
-#export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
+export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
