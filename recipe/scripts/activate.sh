4c9851ad2307623e91bbc8f18f8f8cce40cd4ae9
ngam <67342040+ngam@users.noreply.github.com>
2022-01-05 14:30:20 -0500

    uncomment julia project setting in activate.sh

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -9,6 +9,6 @@ export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
 # Use of @ to specify a shared named environment is new as of Julia 1.7
 # https://github.com/JuliaLang/julia/pull/40025
 # The name of the environment is the last directory of the CONDA_PREFIX
-export JULIA_PROJECT="@${CONDA_PREFIX##*/}"
+# export JULIA_PROJECT="@${CONDA_PREFIX##*/}"
 # Modify load path so that projects stack on the conda-named environment
-export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
+# export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
