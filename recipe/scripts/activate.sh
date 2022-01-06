b7d94e35d7b91f8482ef7419a24dbde3a8415c13
ngam <67342040+ngam@users.noreply.github.com>
2022-01-05 21:12:26 -0500

    Update activate.sh

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -14,4 +14,5 @@ export JULIA_DEPOT_PATH="$CONDA_PREFIX/share/julia:$JULIA_DEPOT_PATH"
 # export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
 
 # make julia use same cert
+export JULIA_SSL_CA_ROOTS_PATH_BACKUP=${JULIA_SSL_CA_ROOTS_PATH:-}
 export JULIA_SSL_CA_ROOTS_PATH=$CONDA_PREFIX/ssl/cacert.pem
