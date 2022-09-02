5a1640d80159fd6fbf09c5efa2cb5f75e35f4408
Mark Kittisopikul <mkitti@users.noreply.github.com>
2022-09-02 07:34:15 -0400

    Do no set JULIA_SSL_CA_ROOTS_PATH on macOS (#221)
    * Do not set JULIA_SSL_CA_ROOTS_PATH on macOS
    
    * Bump build number
    
    * MNT: Re-rendered with conda-build 3.22.0, conda-smithy 3.21.1, and conda-forge-pinning 2022.08.31.16.57.19
    
    * Fix activate.sh and deactivate.sh
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 export JULIA_DEPOT_PATH_BACKUP=${JULIA_DEPOT_PATH:-}
 export JULIA_PROJECT_BACKUP=${JULIA_PROJECT:-}
 export JULIA_LOAD_PATH_BACKUP=${JULIA_LOAD_PATH:-}
@@ -15,7 +17,14 @@ export JULIA_LOAD_PATH="@:$JULIA_PROJECT:@stdlib"
 
 # make julia use same cert
 export JULIA_SSL_CA_ROOTS_PATH_BACKUP=${JULIA_SSL_CA_ROOTS_PATH:-}
-export JULIA_SSL_CA_ROOTS_PATH=$CONDA_PREFIX/ssl/cacert.pem
+if [[ $OSTYPE != 'darwin'* ]]; then
+  export JULIA_SSL_CA_ROOTS_PATH=$CONDA_PREFIX/ssl/cacert.pem
+fi
+# Otherwise, do nothing since we get the following error on macos:
+# Your Julia is built with a SSL/TLS engine that libgit2 doesn't know
+# how to configure to use a file or directory of certificate authority
+# roots.
+
 
 # Setup variables for Conda.jl
 export CONDA_JL_HOME_BACKUP=${CONDA_JL_HOME:-}
