8633d287ef724252e164bc13f3d7ecda74e6cad3
Mark Kittisopikul <markkitt@gmail.com>
2024-02-04 18:22:54 -0500

    Do not set USE_SYSTEM_LIBGIT2=1 while vendoring MBEDTLS

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,8 @@ export EXTRA_MAKEFLAGS=""
 if [[ "${target_platform}" == osx-* ]]; then
     export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=0"
 elif [[ "${target_platform}" == linux-* ]]; then
-    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=1"
+    # USE_SYSTEM_LIBGIT2=1 will not allow MBEDTLS to be vendored
+    # export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=1"
 fi
 # See the following link for how official Julia sets JULIA_CPU_TARGET
 # https://github.com/JuliaCI/julia-buildbot/blob/ba448c690935fe53d2b1fc5ce22bc60fd1e251a7/master/inventory.py
@@ -32,7 +33,7 @@ else
     exit 1
 fi    
 
-export LD_DEBUG="libs"
+#export LD_DEBUG="libs"
 
 make -j${CPU_COUNT} prefix=${PREFIX} sysconfigdir=${PREFIX}/etc \
  LIBBLAS=-lopenblas64_ LIBBLASNAME=libopenblas64_ LIBLAPACK=-lopenblas64_ LIBLAPACKNAME=libopenblas64_ \
