182eff9c4395b5cbf689f099812e5488d048d8c4
ngam <67342040+ngam@users.noreply.github.com>
2024-02-05 11:24:13 -0500

    clean up PR before merge

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,14 @@ EOF
 # Julia sets this to unix makefiles later on in its build process
 export CMAKE_GENERATOR="make"
 
-# On Linux, now autodetects the system libstdc++ version, and automatically loads the system library if it is newer.
-# The old behavior of loading the bundled libstdc++ regardless of the system version can be restored by setting the
-#  environment variable `JULIA_PROBE_LIBSTDCXX=0` ([#46976]).
-export JULIA_PROBE_LIBSTDCXX=0
-
 make -C base version_git.jl.phony CC=$CC CXX=$CXX FC=$FC
+
+export EXTRA_MAKEFLAGS="" 
+if [[ "${target_platform}" == osx-* ]]; then
+    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=0 USE_SYSTEM_MBEDTLS=0"
+elif [[ "${target_platform}" == linux-* ]]; then
+    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=1 USE_SYSTEM_MBEDTLS=1"
+fi
 
 # See the following link for how official Julia sets JULIA_CPU_TARGET
 # https://github.com/JuliaCI/julia-buildbot/blob/ba448c690935fe53d2b1fc5ce22bc60fd1e251a7/master/inventory.py
@@ -29,13 +31,6 @@ elif [[ "${target_platform}" == linux-ppc64le ]]; then
 else
     echo "Unknown target ${target_platform}"
     exit 1
-fi
-
-export EXTRA_MAKEFLAGS="" 
-if [[ "${target_platform}" == osx-* ]]; then
-    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=0 USE_SYSTEM_MBEDTLS=0"
-elif [[ "${target_platform}" == linux-* ]]; then
-    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=1 USE_SYSTEM_MBEDTLS=1"
 fi
 
 make -j${CPU_COUNT} prefix=${PREFIX} sysconfigdir=${PREFIX}/etc \
