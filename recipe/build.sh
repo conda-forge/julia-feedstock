b55feedc8875bea65ab8464c30ac5b62489eaa40
ngam <67342040+ngam@users.noreply.github.com>
2024-02-04 19:34:00 -0500

    try using the vendored libstdc++

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,11 @@ EOF
 # Julia sets this to unix makefiles later on in its build process
 export CMAKE_GENERATOR="make"
 
+# On Linux, now autodetects the system libstdc++ version, and automatically loads the system library if it is newer.
+# The old behavior of loading the bundled libstdc++ regardless of the system version can be restored by setting the
+#  environment variable `JULIA_PROBE_LIBSTDCXX=0` ([#46976]).
+export JULIA_PROBE_LIBSTDCXX=0
+
 make -C base version_git.jl.phony CC=$CC CXX=$CXX FC=$FC
 
 # See the following link for how official Julia sets JULIA_CPU_TARGET
@@ -24,22 +29,25 @@ elif [[ "${target_platform}" == linux-ppc64le ]]; then
 else
     echo "Unknown target ${target_platform}"
     exit 1
-fi    
+fi
 
-#export LD_DEBUG="libs"
+export EXTRA_MAKEFLAGS="" 
+if [[ "${target_platform}" == osx-* ]]; then
+    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=0 USE_SYSTEM_MBEDTLS=0"
+elif [[ "${target_platform}" == linux-* ]]; then
+    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=1 USE_SYSTEM_MBEDTLS=1"
+fi
 
 make -j${CPU_COUNT} prefix=${PREFIX} sysconfigdir=${PREFIX}/etc \
  LIBBLAS=-lopenblas64_ LIBBLASNAME=libopenblas64_ LIBLAPACK=-lopenblas64_ LIBLAPACKNAME=libopenblas64_ \
  USE_SYSTEM_ARPACK=1 \
  USE_SYSTEM_BLAS=1 \
  USE_BLAS64=1 \
- USE_SYSTEM_CURL=0 \
+ USE_SYSTEM_CURL=1 \
  USE_SYSTEM_GMP=1 \
  USE_SYSTEM_LAPACK=1 \
  USE_SYSTEM_LIBSSH2=1 \
  USE_SYSTEM_LLVM=0 \
- USE_SYSTEM_MBEDTLS=0 \
- USE_SYSTEM_LIBGIT2=0 \
  USE_SYSTEM_MPFR=1 \
  USE_SYSTEM_OPENLIBM=1 \
  USE_SYSTEM_PATCHELF=1 \
@@ -52,6 +60,7 @@ make -j${CPU_COUNT} prefix=${PREFIX} sysconfigdir=${PREFIX}/etc \
  USE_SYSTEM_NGHTTP2=1 \
  USE_SYSTEM_ZLIB=1 \
  USE_SYSTEM_P7ZIP=1 \
+ ${EXTRA_MAKEFLAGS} \
  TAGGED_RELEASE_BANNER="https://github.com/conda-forge/julia-feedstock" \
  CC=$CC CXX=$CXX FC=$FC \
  install
