298b82a2c1008a095f1a19ea345e377046dd0fe7
David Lecomber <david+dslarm@lecomber.net>
2025-10-31 22:25:41 +0000

    don't need to specify libmbed in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,9 +13,9 @@ make -C base version_git.jl.phony CC=$CC CXX=$CXX FC=$FC
 
 export EXTRA_MAKEFLAGS="" 
 if [[ "${target_platform}" == osx-* ]]; then
-    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=0 USE_SYSTEM_MBEDTLS=0"
+    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=0"
 elif [[ "${target_platform}" == linux-* ]]; then
-    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=1 USE_SYSTEM_MBEDTLS=1"
+    export EXTRA_MAKEFLAGS="USE_SYSTEM_LIBGIT2=1"
 fi
 
 # See the following link for how official Julia sets JULIA_CPU_TARGET
